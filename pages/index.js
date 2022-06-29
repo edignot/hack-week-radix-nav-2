c92e369f5c4b669d3264dd48eb859baf803c2b70
Edita Ignot <edignot@Editas-MacBook-Pro.local>
2022-06-29 15:29:34 -0600

    Main flexbox container added

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ const NavigationMenuRoot = styled(NavigationMenuPrimitive.Root, {
   display: 'flex',
   flexDirection: 'column',
   width: '100vw',
+  justifyContent: 'center',
+  position: 'fixed',
 })
 
 const NavigationMenuList = styled(NavigationMenuPrimitive.List, {
@@ -20,9 +22,7 @@ const NavigationMenuList = styled(NavigationMenuPrimitive.List, {
   backgroundColor: violet.violet1,
 })
 
-const NavigationMenuItem = styled(NavigationMenuPrimitive.Item, {
-  border: '2px yellow',
-})
+const NavigationMenuItem = styled(NavigationMenuPrimitive.Item, {})
 
 const NavigationMenuTrigger = styled(NavigationMenuPrimitive.Trigger, {
   all: 'unset',
@@ -45,65 +45,96 @@ const StyledCaretDownIcon = styled(CaretDownIcon, {
   },
 })
 
+// MAIN NAV DROPDOWN GRID/FLEX CONTAINER
 const NavigationMenuContent = styled(NavigationMenuPrimitive.Content, {
   borderTop: `1px solid ${violet.violet5}`,
-  border: '2px solid red',
-  padding: '50px',
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignContent: 'center',
+  flexWrap: 'wrap',
+  gap: 100,
+  padding: 50,
 })
 
 const ContentList = styled('ul', {
   border: '2px solid yellow',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: 0,
 })
 
-const NavigationMenuLink = styled(NavigationMenuPrimitive.Link, {
-  display: 'block',
-  margin: 'auto',
-})
+const NavigationMenuLink = styled(NavigationMenuPrimitive.Link, {})
 
 const NavigationMenuViewport = styled(NavigationMenuPrimitive.Viewport, {
   backgroundColor: violet.violet1,
+  boxShadow: `0 0 0 2px ${violet.violet5}`,
+  borderBottomLeftRadius: 25,
+  borderBottomRightRadius: 25,
 })
 
 const Navigation = () => {
   return (
-    <div>
-      <NavigationMenuRoot>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>
-              Product <StyledCaretDownIcon />
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
+    <NavigationMenuRoot value="item1">
+      <NavigationMenuList>
+        <NavigationMenuItem value="item1">
+          <NavigationMenuTrigger>
+            Articles
+            <StyledCaretDownIcon />
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ContentList>
               <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
               <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
               <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          {/* <NavigationMenuItem>
-            <NavigationMenuTrigger>
-              Developers Product <StyledCaretDownIcon />
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink href="#">Product 2</NavigationMenuLink>
-              <NavigationMenuLink href="#">Product 3</NavigationMenuLink>
-              <NavigationMenuLink href="#">Product 4</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>
-              Company Product <StyledCaretDownIcon />
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink href="#">Product 5</NavigationMenuLink>
-              <NavigationMenuLink href="#">Product 6</NavigationMenuLink>
-              <NavigationMenuLink href="#">Product 7</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem> */}
-        </NavigationMenuList>
+            </ContentList>
+            <ContentList>
+              <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
+              <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
+              <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
+            </ContentList>
+            <ContentList>
+              <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
+              <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
+              <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
+            </ContentList>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            Resources
+            <StyledCaretDownIcon />
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink href="#">Product 2</NavigationMenuLink>
+            <NavigationMenuLink href="#">Product 3</NavigationMenuLink>
+            <NavigationMenuLink href="#">Product 4</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            Product <StyledCaretDownIcon />
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink href="#">Product 5</NavigationMenuLink>
+            <NavigationMenuLink href="#">Product 6</NavigationMenuLink>
+            <NavigationMenuLink href="#">Product 7</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            Company <StyledCaretDownIcon />
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink href="#">Product 5</NavigationMenuLink>
+            <NavigationMenuLink href="#">Product 6</NavigationMenuLink>
+            <NavigationMenuLink href="#">Product 7</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
 
-        <NavigationMenuViewport />
-      </NavigationMenuRoot>
-    </div>
+      <NavigationMenuViewport />
+    </NavigationMenuRoot>
   )
 }
 
