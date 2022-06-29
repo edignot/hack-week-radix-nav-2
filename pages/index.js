bac40fe713824394bfe2bd3fa258f770acf3dae3
Edita Ignot <edignot@Editas-MacBook-Pro.local>
2022-06-29 14:04:59 -0600

    Caret animated icon added

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,23 @@
 import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu'
 import { CaretDownIcon } from '@radix-ui/react-icons'
 import { styled, keyframes } from '@stitches/react'
-import { violet, purple, blackA } from '@radix-ui/colors'
+import { violet } from '@radix-ui/colors'
 
 const NavigationMenuRoot = styled(NavigationMenuPrimitive.Root, {
-  border: '1px solid red',
   display: 'flex',
   flexDirection: 'column',
   width: '100vw',
 })
 
 const NavigationMenuList = styled(NavigationMenuPrimitive.List, {
-  border: '2px solid orange',
   display: 'flex',
   gap: 50,
   justifyContent: 'center',
   listStyle: 'none',
-  padding: 0,
   margin: 0,
+  padding: 15,
   width: '100%',
+  backgroundColor: violet.violet1,
 })
 
 const NavigationMenuItem = styled(NavigationMenuPrimitive.Item, {
@@ -26,21 +25,43 @@ const NavigationMenuItem = styled(NavigationMenuPrimitive.Item, {
 })
 
 const NavigationMenuTrigger = styled(NavigationMenuPrimitive.Trigger, {
-  border: '2px solid white',
+  all: 'unset',
+  padding: 10,
+  display: 'flex',
+  fontSize: '1.2rem',
+  color: violet.violet12,
+})
+
+const StyledCaretDownIcon = styled(CaretDownIcon, {
+  width: '1.5rem',
+  height: '1.5rem',
+  color: violet.violet12,
+  '[data-state=open] &': {
+    transform: 'rotate(-180deg)',
+    color: violet.violet7,
+  },
+  '@media (prefers-reduced-motion: no-preference)': {
+    transition: 'transform 250ms ease',
+  },
 })
 
 const NavigationMenuContent = styled(NavigationMenuPrimitive.Content, {
-  border: '2px solid purple',
-  backgroundColor: 'purple',
+  borderTop: `1px solid ${violet.violet5}`,
+  border: '2px solid red',
+  padding: '50px',
+})
+
+const ContentList = styled('ul', {
+  border: '2px solid yellow',
 })
 
 const NavigationMenuLink = styled(NavigationMenuPrimitive.Link, {
-  border: '2px solid blue',
-  dosplay: 'block',
+  display: 'block',
+  margin: 'auto',
 })
 
 const NavigationMenuViewport = styled(NavigationMenuPrimitive.Viewport, {
-  backgroundColor: 'LightGray',
+  backgroundColor: violet.violet1,
 })
 
 const Navigation = () => {
@@ -49,15 +70,19 @@ const Navigation = () => {
       <NavigationMenuRoot>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuTrigger>Product</NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              Product <StyledCaretDownIcon />
+            </NavigationMenuTrigger>
             <NavigationMenuContent>
               <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
               <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
               <NavigationMenuLink href="#">Product 1</NavigationMenuLink>
             </NavigationMenuContent>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Developers</NavigationMenuTrigger>
+          {/* <NavigationMenuItem>
+            <NavigationMenuTrigger>
+              Developers Product <StyledCaretDownIcon />
+            </NavigationMenuTrigger>
             <NavigationMenuContent>
               <NavigationMenuLink href="#">Product 2</NavigationMenuLink>
               <NavigationMenuLink href="#">Product 3</NavigationMenuLink>
@@ -65,13 +90,15 @@ const Navigation = () => {
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <NavigationMenuTrigger>Company</NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              Company Product <StyledCaretDownIcon />
+            </NavigationMenuTrigger>
             <NavigationMenuContent>
               <NavigationMenuLink href="#">Product 5</NavigationMenuLink>
               <NavigationMenuLink href="#">Product 6</NavigationMenuLink>
               <NavigationMenuLink href="#">Product 7</NavigationMenuLink>
             </NavigationMenuContent>
-          </NavigationMenuItem>
+          </NavigationMenuItem> */}
         </NavigationMenuList>
 
         <NavigationMenuViewport />
